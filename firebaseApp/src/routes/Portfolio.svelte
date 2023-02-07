f5ddb7204c99089417cb2f876df1fc985aab19f1
Jack Blake <78292128+JackBlake-zkq@users.noreply.github.com>
2023-02-06 21:04:36 -0600

    Update Portfolio.svelte

--- firebaseApp/src/routes/Portfolio.svelte
+++ firebaseApp/src/routes/Portfolio.svelte
@@ -7,18 +7,18 @@
 <PageTransition>
     <main>
         <div>
+            <ProjectCard imageURL="https://i.imgur.com/FjbNBBi.gif" name="Data Science for Sustainable Development - Madison Hub Website" 
+                desc="Content focussed website that I acted as Project Manager for. Uses Contentful, a Headless CMS, and Astro for partial SSR."
+                links={[["Website", "https://madison.dssdglobal.org/"], ["GitHub", "https://github.com/DSSD-Madison/madison-website"]}/>
             <ProjectCard imageURL="https://i.imgur.com/FjbNBBi.gif" name="Eirene Peacebuilding Database" 
                 desc="Web app I helped build for Alliance for Peacebuilding during my internship with Data Science for Sustainbable Development (DSSD) Global. Allows public officials to search for measurements that can help them prove the effectiveness of their peacebuilding policies. Check out my Resume to learn more about my role!"
                 links={[["Website", "https://eirene.allianceforpeacebuilding.org/#/login"]]}/>
-            <ProjectCard imageURL="https://i.imgur.com/mGbty4Y.gif" name="slant.pro" 
+            <ProjectCard imageURL="https://i.imgur.com/mGbty4Y.gif" name="Slant" 
                 desc="Browser game where users can create and play levels where they must bounce a ball into a goal gaurded by any number of walls. User experience inpired by Geometry Dash."
-                links={[["Website", "http://www.slant.pro/"], ["GitHub", "https://github.com/JackBlake-zkq/slant-browser-game"]]}/>
+                links={[["Website", "https://sl4nt.herokuapp.com/"], ["GitHub", "https://github.com/JackBlake-zkq/slant-browser-game"]]}/>
             <ProjectCard imageURL="https://i.imgur.com/0UpIhZf.gif" name="Break Free From Plastic" 
                 desc="Web app that tracks plastic usage avoided through reuses systems at 25 stores in the Philippines. Check out my Resume to learn more about my role!"
                 links={[["Website", "https://bffp-backend.web.app/"]]}/>
-            <ProjectCard imageURL="https://i.imgur.com/QtKEVQm.gif" name="McNally Fans WIP" 
-                desc="Novelty merchandise shop where users can order mugs, stickers, and more products to come to their doorsteps. Merchandise is themed around a beloved Physics teacher from Glen Rock High School."
-                links={[["Website", "https://mcnally.fans/"], ["GitHub", "https://github.com/JackBlake-zkq/novelty-merch-shop"]]}/>
         </div>
         <h4>
             Check out some of my older projects on <a href="https://jackblake-zkq.github.io/old-personal-website/">my old website</a>!
