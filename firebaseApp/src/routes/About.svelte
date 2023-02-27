66ff8296b42a97f5ff676942cebbb1feb6742c1e
Jack Blake <78292128+JackBlake-zkq@users.noreply.github.com>
2023-02-27 15:30:55 -0600

    Update About.svelte

--- firebaseApp/src/routes/About.svelte
+++ firebaseApp/src/routes/About.svelte
@@ -110,7 +110,7 @@
 
     const about = [
         "Hi! I'm <b>Jack Blake</b> 👋",
-        "I'm a computer science major at University of Wisconsin-Madison graduating next fall. I'm interested in theoretical computer science and web development (escpecially backend). I'm planning on getting a masters in computer science and pursuing a career in mission driven web development.",
+        "I'm a computer science major at University of Wisconsin-Madison graduating next fall. I'm interested in software development and system design. I'm planning on getting a masters in computer science and pursuing a career in mission driven development.",
         "When I'm not coding, you can usually find me playing table tennis, enjoying a game of Magic: The Gathering (EDH), or watching a movie."
     ]
         
