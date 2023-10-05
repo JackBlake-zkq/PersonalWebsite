30f48303566d1f816ed1af4f1e53ca4022a080d3
Jack Blake <78292128+JackBlake-zkq@users.noreply.github.com>
2023-10-05 12:20:41 -0500

    Update Portfolio.svelte

--- firebaseApp/src/routes/Portfolio.svelte
+++ firebaseApp/src/routes/Portfolio.svelte
@@ -11,14 +11,14 @@
                 desc="Chrome extension for UW Madison students that pulls Rate My Professors and Madgrades info into Course Search & Enroll"
                 links={[["Website", "https://chrome.google.com/webstore/detail/uw-madison-course-search/ldnllmdimjknflobmdjnmefeollalodf"],["GitHub", "https://github.com/JackBlake-zkq/cse-plus"]]}/>
             <ProjectCard imageURL="https://images.ctfassets.net/x00i2tlytr27/6RmXcRxOTji0flKJf4JaSr/92d81414981ae9d742ad39e116cfcb95/Screen_Shot_2023-05-21_at_3.05.10_PM.png" name="UW Madison - Solar Array Analytics" 
-                desc="DSSD collaborated with Helios to create an analytics page for the solar arrays on UW-Madison's campus"
+                desc="Analytics page for the solar arrays on UW-Madison's campus, and the estimated benefit of proposed new arrays"
                 links={[["Website", "https://madison-solar.dssdglobal.org/"],["GitHub", "https://github.com/DSSD-Madison/Helios"]]}/>
-            <ProjectCard imageURL="https://i.imgur.com/fYnmy3o.png" name="ROBUG" 
-                desc="Real time multi-player browser game made for Madhacks 2023"
-                links={[["Website", "https://madhacks-io-game-hhwbvukxcq-uc.a.run.app/"],["GitHub", "https://github.com/JackBlake-zkq/madhacks-io-game"]]}/>
             <ProjectCard imageURL="https://i.imgur.com/KyJMRPw.jpg" name="Data Science for Sustainable Development - Madison Hub Website" 
-                desc="Content focussed website that I acted as Project Manager for. Uses Contentful, a Headless CMS, and Astro for partial SSR."
+                desc="Open-source framework for creating websites for student organizations. Made with Contentful, a Headless CMS, and Astro for partial SSR"
                 links={[["Website", "https://madison.dssdglobal.org/"], ["GitHub", "https://github.com/DSSD-Madison/madison-website"]]}/>
+            <ProjectCard imageURL="https://i.imgur.com/fYnmy3o.png" name="ROBUG" 
+                desc="Real-time multi-player browser game made for Madhacks 2023"
+                links={[["Website", "https://madhacks-io-game-hhwbvukxcq-uc.a.run.app/"],["GitHub", "https://github.com/JackBlake-zkq/madhacks-io-game"]]}/>
             <ProjectCard imageURL="https://i.imgur.com/LjyRGep.gif" name="Current Personal Website (This Website)" 
                 desc="Newest iteration of my personal website, with more focus on SEO (still client rendered but Google's crawlers can see most important content) and interactivity. It's a single page app created with Svelte for the frontend and Firebase for some light backend functionality."
                 links={[["GitHub", "https://github.com/JackBlake-zkq/personal-website-1"]]}/>
@@ -29,7 +29,7 @@
                 desc="Browser game where users can create and play levels where they must bounce a ball into a goal gaurded by any number of walls. User experience inpired by Geometry Dash."
                 links={[["Website", "https://slant-k2h52ozsya-uc.a.run.app"], ["GitHub", "https://github.com/JackBlake-zkq/slant-browser-game"]]}/>
             <ProjectCard imageURL="https://i.imgur.com/0UpIhZf.gif" name="Break Free From Plastic" 
-                desc="Web app that tracks plastic usage avoided through reuses systems at 25 stores in the Philippines. Check out my Resume to learn more about my role!"
+                desc="Web app that tracks plastic usage avoided through reuse systems at 25 stores in the Philippines."
                 links={[["Website", "https://bffp-backend.web.app/"]]}/>
             <ProjectCard imageURL="https://i.imgur.com/hEjoOMl.gif" name="Old Personal Website" 
                 desc="The first iteration of my personal website. Made with vanilla Javascript, HTML & CSS"
